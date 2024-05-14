18a9d0f00845cbb2a2ef509a764a49838b6a5797
Lettie Galindo <lelegalindo925@gmail.com>
2024-05-14 06:51:25 -0700

    Spring Security

--- bookapp/src/main/java/com/bookappproject/bookapp/BookappApplication.java
+++ bookapp/src/main/java/com/bookappproject/bookapp/BookappApplication.java
@@ -1,8 +1,11 @@
 package com.bookappproject.bookapp;
 
+import com.bookappproject.bookapp.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class BookappApplication {
 
